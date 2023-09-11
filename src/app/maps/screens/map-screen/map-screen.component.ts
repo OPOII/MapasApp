9ed9299f5f79aba3dcf7ee6157a95abd69227e6a
OPOII <bryancamili198@hotmail.com>
2023-09-11 09:26:30 -0500

    obtener la geolocalización del usuario

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PlacesService } from '../../services/places.service';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
   styleUrls: ['./map-screen.component.css']
 })
-export class MapScreenComponent {
+export class MapScreenComponent implements OnInit {
+
+  private placesServices=inject(PlacesService)
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+if(!navigator.geolocation){
+  alert('No hay geolocalización')
+  throw new Error('No hay geolocalización');
+}
+
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
