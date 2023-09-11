62fe32cd46dc70fdc134186e34ca8d4f1ef84671
OPOII <bryancamili198@hotmail.com>
2023-09-11 11:15:33 -0500

    Mostrar un mapa de mapbox

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,17 +1,24 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
-
+import { Map } from 'mapbox-gl';
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit{
+export class MapViewComponent implements AfterViewInit{
 
+  @ViewChild('mapDiv')
+  mapDivElement!:ElementRef
   private placesService=inject(PlacesService);
 
-  ngOnInit(): void {
-    console.log(this.placesService.userLocation)
-    // throw new Error('Method not implemented.');
+  ngAfterViewInit(): void {
+    if(!this.placesService.userLocation)throw Error('No hay placesServices.userLocation')
+    const map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/dark-v11', // style URL
+      center: this.placesService.userLocation, // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1Ijoib3BvaWkiLCJhIjoiY2xsZmk1bWxzMHA0ODNscGp4dTc1YXMxYyJ9.4W2-nZ7oilWcDMjYg3CyCw';
+
 if(!navigator.geolocation){
   alert('No hay geolocalización')
   throw new Error('No hay geolocalización');
