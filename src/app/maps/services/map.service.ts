2aad96e1cb52913ce360fbb4091f0386763db0a8
OPOII <bryancamili198@hotmail.com>
2023-09-12 15:04:36 -0500

    ajustar el mapa para mostrar todos los marcadores encontrados mas el lugar del usuario

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places';
 
 @Injectable({
@@ -26,24 +26,33 @@ export class MapService {
       center:cords
     })
   }
-  createMarkersFromPlaces(places:Feature[]){
+  createMarkersFromPlaces(places:Feature[], userLocation:[number,number]){
     if(!this.map)throw Error('Mapa no inicializado')
     this.markers.forEach(marker=>marker.remove());
-    const newMarkers=[];
-    for(const place of places){
-      const [lng,lat]=place.center;
-      const popup=new Popup()
-      .setHTML(`
-        <h6>${place.text}</h6>
-        <span>${place.place_name}</span>
-      `);
-      const newMarker=new Marker()
-      .setLngLat([lng,lat])
-      .setPopup(popup)
-      .addTo(this.map)
+  const newMarkers=[];
+  for(const place of places){
+    const [lng,lat]=place.center;
+    const popup=new Popup()
+    .setHTML(`
+    <h6>${place.text}</h6>
+    <span>${place.place_name}</span>
+    `);
+    const newMarker=new Marker()
+    .setLngLat([lng,lat])
+    .setPopup(popup)
+    .addTo(this.map)
 
-      newMarkers.push(newMarker)
-    }
-    this.markers=newMarkers
+    newMarkers.push(newMarker)
+  }
+  this.markers=newMarkers;
+  if(places.length===0)return;
+
+    //limites mapas
+    const bounds=new LngLatBounds()
+    newMarkers.forEach(marker=>bounds.extend(marker.getLngLat()));
+    bounds.extend(userLocation)
+    this.map.fitBounds(bounds,{
+      padding:200
+    })
   }
 }
