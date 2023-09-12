5dc2852706c5a9ccd3cf5f279eba722ee9a3cd07
OPOII <bryancamili198@hotmail.com>
2023-09-12 15:57:50 -0500

    Ocultar el menu de lugares

--- src/app/maps/components/search-results/search-results.component.ts
+++ src/app/maps/components/search-results/search-results.component.ts
@@ -30,8 +30,9 @@ export class SearchResultsComponent {
 
   getDirections(place:Feature){
     if(!this.placesService.userLocation)throw Error('No hay userLocation')
+    this.placesService.deletePlaces()
     const start=this.placesService.userLocation;
     const end=place.center as [number,number];
-    this.mapService.getRouteBetweenPoints(start,end)
+    this.mapService.getRouteBetweenPoints(start,end);
   }
 }
